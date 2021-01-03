196ccf07f5c3a1eed67c216a1460771bfc9db2d2
Aldo Surya Ongko <skyaironline@gmail.com>
2021-01-03 17:56:23 +0700

    Add loading animation in image
    Signed-off-by: Aldo Surya Ongko <skyaironline@gmail.com>

--- app/src/main/java/com/arudo/blown/core/utils/BackgroundImageContainer.kt
+++ app/src/main/java/com/arudo/blown/core/utils/BackgroundImageContainer.kt
@@ -2,12 +2,19 @@ package com.arudo.blown.core.utils
 
 import android.content.Context
 import android.widget.ImageView
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import com.arudo.blown.R
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 
 fun BackgroundImageContainer(context: Context, imagePath: String, view: ImageView) {
+    val circularProgressDrawable = CircularProgressDrawable(context)
+    circularProgressDrawable.centerRadius = 10f
+    circularProgressDrawable.strokeWidth = 5f
+    circularProgressDrawable.start()
+
     Glide.with(context).load(imagePath).apply(
-        RequestOptions.overrideOf(640, 320).error(R.drawable.ic_broken_image)
+        RequestOptions.overrideOf(640, 360).placeholder(circularProgressDrawable).centerCrop()
+            .error(R.drawable.ic_broken_image)
     ).into(view)
 }
